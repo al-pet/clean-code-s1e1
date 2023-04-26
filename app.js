34a3447a0956095a308215695b3952909afdbc85
al-pet <apetrov1024@gmail.com>
2023-04-26 12:04:51 +0300

    Revert "Merge branch 'clean-code-s1e1' of https://github.com/al-pet/clean-code-s1e1 into clean-code-s1e1"
    This reverts commit aff01b3cc8fddeee5713de2d27af1acf4bf49714, reversing
    changes made to 4a17c435044ff6ec20c0268e4d1827defd7bba81.

--- app.js
+++ app.js
@@ -187,13 +187,11 @@ for (var i = 0; i < incompleteTaskHolder.children.length; i++) {
 
 
 
-
 //cycle over completedTasksHolder ul list items
 for (var i = 0; i < completedTasksHolder.children.length; i++) {
   //bind events to list items chldren(tasksIncompleted)
   bindTaskEvents(completedTasksHolder.children[i], taskIncomplete);
 }
-
 
 
 
